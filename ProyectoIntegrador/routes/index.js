47f01842e88e3b3dc1e1eaf578efc47f840055ac
Francisco Iribarne <pacoiribarne@MacBook-Air-de-Francisco-2.local>
2021-09-13 11:20:14 -0300

    rutas

--- ProyectoIntegrador/routes/index.js
+++ ProyectoIntegrador/routes/index.js
@@ -5,6 +5,31 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-
+router.get('/login', function(req, res, next){
+  res.render('login', {title : "Express"});
+});
+router.get('/registracion', function(req, res, next){
+  res.render('registracion', {title : "Express"});
+});
+router.get('/resultadoBusqueda', function(req, res, next){
+  res.render('resultadoBusqueda', {title : "Express"});
+});
+router.get('/login', function(req, res, next){
+  res.render('login', {title : "Express"});
+});
+router.get('/miperfil', function(req, res, next){
+  res.render('miperfil', {title : "Express"});
+});
+router.get('/agregarPost', function(req, res, next){
+  res.render('agregarPost', {title : "Express"});
+});
+router.get('/detallePost', function(req, res, next){
+  res.render('detallePost', {title : "Express"});
+});
+router.get('/detalleUsuario', function(req, res, next){
+  res.render('detalleUsuario', {title : "Express"});
+});
+router.get('/editarPerfil', function(req, res, next){
+  res.render('editarPerfil', {title : "Express"});
+});
 module.exports = router;
