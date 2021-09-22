21481788fc57a76fe20cb1fb902a093155244188
lorenzofal <63105821+lorenzofal@users.noreply.github.com>
2021-09-22 09:15:19 -0300

    home hecho

--- ProyectoIntegrador/data/home.js
+++ ProyectoIntegrador/data/home.js
@@ -2,37 +2,37 @@ const home = {
     posts : [
         {
             id : 0,
-            usuario : "Arepa",
+            usuario : "Agustin Patio",
             imagen : "comida4.jpeg",
             pie : "balablabla",
         },
         {
             id : 1,
-            usuario : "loca",
+            usuario : "Martin Palacio",
             pie : "Tequeños",
             imagen : "comida5.jpeg",
         },
         {
             id : 2,
-            usuario : "Arepa",
+            usuario : "Justina Dominguez",
             pie : "Asado negro",
             imagen : "comida6.jpeg",
         },
         {
             id : 3,
-            usuario : "Arepa",
+            usuario : "Charly Pintos",
             pie : "Pizza",
             imagen : "comida3.jpeg",
         },
         {
             id : 4,
-            usuario : "Arepa",
+            usuario : "Latin_lover_0101",
             pie : "Sushi",
             imagen : "comida2.jpeg",
         },
         {
             id : 5,
-            usuario : "Arepa",
+            usuario : "Jorge Luis Borges",
             pie : "Hamburguesa",
             imagen : "comida1.jpeg",
         },
