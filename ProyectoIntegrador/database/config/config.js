2113470a308776e7b78ed23eaa671784a1e5202b
CarmelaMartinezMori <cmartinezmori@udesa.edu.ar>
2021-10-21 09:30:48 -0300

    conexión a la base de datos

--- ProyectoIntegrador/database/config/config.js
+++ ProyectoIntegrador/database/config/config.js
@@ -1,10 +1,11 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": "root",
+    "database": "foodloverz",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "operatorsAliases": false
   },
   "test": {
     "username": "root",
