3e79a1be4f6cb8db30502cf318940dc775077278
CarmelaMartinezMori <cmartinezmori@udesa.edu.ar>
2021-11-04 09:54:19 -0300

    updated at y created at

--- ProyectoIntegrador/db/database.sql
+++ ProyectoIntegrador/db/database.sql
@@ -7,7 +7,9 @@ email VARCHAR (50) UNIQUE NOT NULL,
 contraseña VARCHAR (500) NOT NULL,
 fechaDeNacimiento  DATE NOT NULL,
 fotoPerfil VARCHAR (200) NOT NULL,
-celular INT NOT NULL
+celular INT NOT NULL,
+createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -19,7 +21,8 @@ imagen VARCHAR (200) NOT NULL,
 fotoPerfil VARCHAR (200) NOT NULL,
 fecha DATE NOT NULL,
 usuarios_id INT UNSIGNED NOT NULL,
-
+createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (usuarios_id) REFERENCES usuarios (id)
 );
 
@@ -30,144 +33,145 @@ texto VARCHAR (500) NOT NULL,
 usuarios_id INT UNSIGNED NOT NULL,
 posteos_id INT UNSIGNED NOT NULL,
 creacion DATE NOT NULL,
-
+createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (posteos_id) REFERENCES posteos (id),
 FOREIGN KEY (usuarios_id) REFERENCES usuarios (id)
 );
 
 
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Martin', 'Palacio', 'martinpalacio', 'marpalacio@hotmail.com',  'Martin1234', '1999-07-14', 'foto2.jpg', 153586000);
+VALUES (DEFAULT, 'Martin', 'Palacio', 'martinpalacio', 'marpalacio@hotmail.com',  'Martin1234', '1999-07-14', 'foto2.jpg', 153586000, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Charly', 'Pintos', 'charlyypintos', 'pintoscharly@gmail.com',  'Charly1234', '1997-10-03', 'foto4.jpg', 153586001);
+VALUES (DEFAULT, 'Charly', 'Pintos', 'charlyypintos', 'pintoscharly@gmail.com',  'Charly1234', '1997-10-03', 'foto4.jpg', 153586001, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Justina', 'Dominguez', 'justidominguezz', 'justinadom@gmail.com',  'Justina1234', '2000-12-19', 'foto3.jpg', 153586001);
+VALUES (DEFAULT, 'Justina', 'Dominguez', 'justidominguezz', 'justinadom@gmail.com',  'Justina1234', '2000-12-19', 'foto3.jpg', 153586001, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Manolo', 'Devalle', 'latin_lover_0101', 'mlover@hotmail.com',  'Manolo1234', '2000-07-05', 'foto5.jpg', 153586002);
+VALUES (DEFAULT, 'Manolo', 'Devalle', 'latin_lover_0101', 'mlover@hotmail.com',  'Manolo1234', '2000-07-05', 'foto5.jpg', 153586002, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Jorge Luis', 'Borges', 'jlborges', 'juanluisb86@hotmail.com',  'Juanluis1234', '1999-08-24', 'foto6.jpg', 153586003);
+VALUES (DEFAULT, 'Jorge Luis', 'Borges', 'jlborges', 'juanluisb86@hotmail.com',  'Juanluis1234', '1999-08-24', 'foto6.jpg', 153586003, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Carlos', 'Ramirez', 'cramirez', 'carlitos@gmail.com',  'Carlos1234', '1997-03-24', 'foto7.jpg', 153586004);
+VALUES (DEFAULT, 'Carlos', 'Ramirez', 'cramirez', 'carlitos@gmail.com',  'Carlos1234', '1997-03-24', 'foto7.jpg', 153586004, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Martina', 'Suarez', 'martuuu_', 'martinasuarez@gmail.com',  'Martu1234', '2002-02-11', 'foto8.jpg', 153586005);
+VALUES (DEFAULT, 'Martina', 'Suarez', 'martuuu_', 'martinasuarez@gmail.com',  'Martu1234', '2002-02-11', 'foto8.jpg', 153586005, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Sofia', 'Vazquez', 'sofivazquex', 'sofiaav@hotmail.com',  'Sofia1234', '1997-11-27', 'foto9.jpg', 153586006);
+VALUES (DEFAULT, 'Sofia', 'Vazquez', 'sofivazquex', 'sofiaav@hotmail.com',  'Sofia1234', '1997-11-27', 'foto9.jpg', 153586006, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Santiago', 'Pons', 'santipons', 'santiagopons@gmail.com',  'Santi1234', '1999-08-10', 'foto10.jpg', 153586007);
+VALUES (DEFAULT, 'Santiago', 'Pons', 'santipons', 'santiagopons@gmail.com',  'Santi1234', '1999-08-10', 'foto10.jpg', 153586007, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Juana', 'Lopez', 'juanitalopez', 'juanalo@gmail.com',  'Juana1234', '2001-10-03', 'foto1.jpg', 153586009);
+VALUES (DEFAULT, 'Juana', 'Lopez', 'juanitalopez', 'juanalo@gmail.com',  'Juana1234', '2001-10-03', 'foto1.jpg', 153586009, DEFAULT, DEFAULT);
 INSERT INTO usuarios
-VALUES (DEFAULT, 'Carlos', 'Baute', 'carlitos', 'carlosbaute@gmail.com',  'Carlos1234', '1974-03-08', 'carlosbaute2.jpeg', 153586008);
+VALUES (DEFAULT, 'Carlos', 'Baute', 'carlitos', 'carlosbaute@gmail.com',  'Carlos1234', '1974-03-08', 'carlosbaute2.jpeg', 153586008, DEFAULT, DEFAULT);
 
 
 
 INSERT INTO posteos
-VALUES (DEFAULT, 'martinpalacio', 'Tequeños <3', 'comida1.jpeg', 'foto2.jpg', '2021-09-24', 1);
+VALUES (DEFAULT, 'martinpalacio', 'Tequeños <3', 'comida1.jpeg', 'foto2.jpg', '2021-09-24', 1, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'justidominguez', 'Una sopita', 'comida2.jpg', 'foto3.jpg', '2021-09-24', 3);
+VALUES (DEFAULT, 'justidominguez', 'Una sopita', 'comida2.jpg', 'foto3.jpg', '2021-09-24', 3, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'charlyypintos', 'Pizza', 'comida3.jpg', 'foto4.jpg', '2021-09-22', 2);
+VALUES (DEFAULT, 'charlyypintos', 'Pizza', 'comida3.jpg', 'foto4.jpg', '2021-09-22', 2, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'latin_lover_010', 'Sushi', 'comida4.jpg', 'foto5.jpg', '2021-09-26', 4);
+VALUES (DEFAULT, 'latin_lover_010', 'Sushi', 'comida4.jpg', 'foto5.jpg', '2021-09-26', 4, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'jlborges', 'Hamburguesa', 'comida5.jpg', 'foto6.jpg', '2021-09-27', 5);
+VALUES (DEFAULT, 'jlborges', 'Hamburguesa', 'comida5.jpg', 'foto6.jpg', '2021-09-27', 5, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'cramirez', 'Cocinando nuevas cosas', 'comida6.jpg', 'foto7.jpg', '2021-09-25', 6);
+VALUES (DEFAULT, 'cramirez', 'Cocinando nuevas cosas', 'comida6.jpg', 'foto7.jpg', '2021-09-25', 6, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'martuuu_', 'Cupcakes de vainilla', 'comida7.jpg', 'foto8.jpg', '2021-09-21', 7);
+VALUES (DEFAULT, 'martuuu_', 'Cupcakes de vainilla', 'comida7.jpg', 'foto8.jpg', '2021-09-21', 7, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'sofivazquex', 'Pasando el calor', 'comida8.jpg', 'foto9.jpg', '2021-09-23', 8);
+VALUES (DEFAULT, 'sofivazquex', 'Pasando el calor', 'comida8.jpg', 'foto9.jpg', '2021-09-23', 8, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'santipons', 'Pancakes caseros', 'comida9.jpg', 'foto10.jpg', '2021-09-24', 9);
+VALUES (DEFAULT, 'santipons', 'Pancakes caseros', 'comida9.jpg', 'foto10.jpg', '2021-09-24', 9, DEFAULT, DEFAULT);
 INSERT INTO posteos
-VALUES (DEFAULT, 'juanitalopez', 'Sanguchito', 'comida0.jpg', 'foto1.jpg', '2021-09-25', 10);
+VALUES (DEFAULT, 'juanitalopez', 'Sanguchito', 'comida0.jpg', 'foto1.jpg', '2021-09-25', 10, DEFAULT, DEFAULT);
 
 
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martinpalacio', 'Oh, se ve delicioso!', 1, 10, '2021-09-25');
+VALUES (DEFAULT, 'martinpalacio', 'Oh, se ve delicioso!', 1, 10, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'charlyypintos' , 'No mientas, eso es photoshop', 2, 10, '2021-09-25');
+VALUES (DEFAULT, 'charlyypintos' , 'No mientas, eso es photoshop', 2, 10, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'justidominguezz', 'Con ese plato puedes chantajear a Shakira, jaja', 3, 10, '2021-09-25');
+VALUES (DEFAULT, 'justidominguezz', 'Con ese plato puedes chantajear a Shakira, jaja', 3, 10, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'latin_lover_0101' , 'Hay que darle de comer a quienes mas lo necesitan.', 4, 10, '2021-09-25');
+VALUES (DEFAULT, 'latin_lover_0101' , 'Hay que darle de comer a quienes mas lo necesitan.', 4, 10, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'jlborges' , 'Que ricura por favorr', 5, 1, '2021-09-23');
+VALUES (DEFAULT, 'jlborges' , 'Que ricura por favorr', 5, 1, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'cramirez' , 'Se ve espectacular', 6, 1, '2021-09-24');
+VALUES (DEFAULT, 'cramirez' , 'Se ve espectacular', 6, 1, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martuuu_' , 'Queremos comer', 7, 1, '2021-09-24');
+VALUES (DEFAULT, 'martuuu_' , 'Queremos comer', 7, 1, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'sofivazquex', 'Me encantaría la receta!!!', 8, 1, '2021-09-24');
+VALUES (DEFAULT, 'sofivazquex', 'Me encantaría la receta!!!', 8, 1, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'santipons', 'Que rico! Dónde se consigue?', 9, 2, '2021-09-23');
+VALUES (DEFAULT, 'santipons', 'Que rico! Dónde se consigue?', 9, 2, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'juanitalopez', 'La receta?', 10, 2, '2021-09-23');
+VALUES (DEFAULT, 'juanitalopez', 'La receta?', 10, 2, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'carlitos', 'ARTE PURO!!', 11, 2, '2021-09-22');
+VALUES (DEFAULT, 'carlitos', 'ARTE PURO!!', 11, 2, '2021-09-22', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martinpalacio', 'Quiero recetaa!! ', 1, 2, '2021-09-23');
+VALUES (DEFAULT, 'martinpalacio', 'Quiero recetaa!! ', 1, 2, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'charlyypintos', 'Wow. Esto es genial.', 2, 3, '2021-09-23');
+VALUES (DEFAULT, 'charlyypintos', 'Wow. Esto es genial.', 2, 3, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'justidominguezz', 'Yum', 3, 3, '2021-09-23');
+VALUES (DEFAULT, 'justidominguezz', 'Yum', 3, 3, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'latin_lover_0101', 'Muy bueno, pero tienen todas las personas la igualdad de acceder a recursos?', 4, 3, '2021-09-23');
+VALUES (DEFAULT, 'latin_lover_0101', 'Muy bueno, pero tienen todas las personas la igualdad de acceder a recursos?', 4, 3, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'jlborges', 'Hola, saludos', 5, 3, '2021-09-23');
+VALUES (DEFAULT, 'jlborges', 'Hola, saludos', 5, 3, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'cramirez', 'Que deliciaa', 6, 4, '2021-09-26');
+VALUES (DEFAULT, 'cramirez', 'Que deliciaa', 6, 4, '2021-09-26', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martuuu_', 'Que genial esta foto', 7, 4, '2021-09-26');
+VALUES (DEFAULT, 'martuuu_', 'Que genial esta foto', 7, 4, '2021-09-26', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'sofivazquex', 'que bueno se ve!', 8, 4, '2021-09-26');
+VALUES (DEFAULT, 'sofivazquex', 'que bueno se ve!', 8, 4, '2021-09-26', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'santipons', 'Que hermoso, bendiciones ', 9, 4, '2021-09-26');
+VALUES (DEFAULT, 'santipons', 'Que hermoso, bendiciones ', 9, 4, '2021-09-26', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'juanitalopez', 'Quierooo!!!', 10, 5, '2021-09-27');
+VALUES (DEFAULT, 'juanitalopez', 'Quierooo!!!', 10, 5, '2021-09-27', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'carlitos', 'Sabroso', 11, 5, '2021-09-27');
+VALUES (DEFAULT, 'carlitos', 'Sabroso', 11, 5, '2021-09-27', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martinpalacio', 'Espectacularr!! ', 1, 5, '2021-09-27');
+VALUES (DEFAULT, 'martinpalacio', 'Espectacularr!! ', 1, 5, '2021-09-27', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'charlyypintos', 'que ricooo', 2, 5, '2021-09-27');
+VALUES (DEFAULT, 'charlyypintos', 'que ricooo', 2, 5, '2021-09-27', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'justidominguezz', 'Estoy esperando la receta! :)', 3, 6, '2021-09-25');
+VALUES (DEFAULT, 'justidominguezz', 'Estoy esperando la receta! :)', 3, 6, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'latin_lover_0101', 'Me mueroo, que rico', 4, 6, '2021-09-25');
+VALUES (DEFAULT, 'latin_lover_0101', 'Me mueroo, que rico', 4, 6, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'jlborges', 'Dónde puedo conseguir esto?!', 5, 6, '2021-09-25');
+VALUES (DEFAULT, 'jlborges', 'Dónde puedo conseguir esto?!', 5, 6, '2021-09-25', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'cramirez', 'Me Encanta <3', 6, 6, '2021-09-26');
+VALUES (DEFAULT, 'cramirez', 'Me Encanta <3', 6, 6, '2021-09-26', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martuuu_', 'Divina la foto', 7, 7, '2021-09-21');
+VALUES (DEFAULT, 'martuuu_', 'Divina la foto', 7, 7, '2021-09-21', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'sofivazquex', 'Woww, me muero por probar eso', 8, 7, '2021-09-21');
+VALUES (DEFAULT, 'sofivazquex', 'Woww, me muero por probar eso', 8, 7, '2021-09-21', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'santipons', 'muy bueno', 9, 7, '2021-09-22');
+VALUES (DEFAULT, 'santipons', 'muy bueno', 9, 7, '2021-09-22', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'juanitalopez', 'Espectacular es poco!!!', 10, 7, '2021-09-23');
+VALUES (DEFAULT, 'juanitalopez', 'Espectacular es poco!!!', 10, 7, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'carlitos', 'Ríquisimo!', 11, 8, '2021-09-23');
+VALUES (DEFAULT, 'carlitos', 'Ríquisimo!', 11, 8, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martinpalacio', 'Muy bueno', 1, 8, '2021-09-23');
+VALUES (DEFAULT, 'martinpalacio', 'Muy bueno', 1, 8, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'charlyypintos', 'un manjar', 2, 8, '2021-09-23');
+VALUES (DEFAULT, 'charlyypintos', 'un manjar', 2, 8, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'justidominguezz', 'me dio hambre', 3, 8, '2021-09-23');
+VALUES (DEFAULT, 'justidominguezz', 'me dio hambre', 3, 8, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'latin_lover_0101', 'que bien se ve esto', 4, 8, '2021-09-23');
+VALUES (DEFAULT, 'latin_lover_0101', 'que bien se ve esto', 4, 8, '2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'jlborges', 'Genial!!!', 5, 9, '2021-09-24');
+VALUES (DEFAULT, 'jlborges', 'Genial!!!', 5, 9, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'cramirez', 'muy buenas tus publicaciones', 6, 9, '2021-09-24');
+VALUES (DEFAULT, 'cramirez', 'muy buenas tus publicaciones', 6, 9, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'martuuu_', 'qué restaurante es?', 7, 9, '2021-09-24');
+VALUES (DEFAULT, 'martuuu_', 'qué restaurante es?', 7, 9, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'sofivazquex', 'Woww', 8, 9, '2021-09-24');
+VALUES (DEFAULT, 'sofivazquex', 'Woww', 8, 9, '2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'santipons', 'Exquisitoo', 9, 10,'2021-09-24');
+VALUES (DEFAULT, 'santipons', 'Exquisitoo', 9, 10,'2021-09-24', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'juanitalopez', 'Me tentaste, ya me voy a comprar para comer', 10, 1,'2021-09-23');
+VALUES (DEFAULT, 'juanitalopez', 'Me tentaste, ya me voy a comprar para comer', 10, 1,'2021-09-23', DEFAULT, DEFAULT);
 INSERT INTO comentarios
-VALUES (DEFAULT, 'carlitos', 'Que rico', 11, 10,'2021-09-25');
+VALUES (DEFAULT, 'carlitos', 'Que rico', 11, 10,'2021-09-25', DEFAULT, DEFAULT);
