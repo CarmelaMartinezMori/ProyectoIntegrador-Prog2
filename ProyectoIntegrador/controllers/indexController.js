3f0d3e80a9bcb9e416b28b5ebf3cd822aba256a4
Francisco Iribarne <pacoiribarne@MacBook-Air-de-Francisco-2.local>
2021-11-15 10:12:50 -0300

    search

--- ProyectoIntegrador/controllers/indexController.js
+++ ProyectoIntegrador/controllers/indexController.js
@@ -3,7 +3,9 @@ const posteo = db.Posteo;
 const comentario = db.Comentario;
 const usuario = db.Usuario;
 
+
 const op = db.Sequelize.Op
+
 
 const indexController = {
     //HOME
@@ -22,18 +24,20 @@ const indexController = {
         })
     }, 
     //BUSCADOR
-    search: (req, res) => {
-        let search = req.query.search;
+    search: function (req, res) {
+        let search = req.query.busqueda;
         posteo.findAll({
             where: [
-                {"pie":  {[op.like]:`%${search}%`}}
-            ]
+                {'pie':  {[op.like]: `%${search}%`}}
+            ],
+            limit: 10,
+            offset: 0,
         })
         .then (resultado => {
             res.render('resultadoBusqueda', {pie: resultado});
         })
         
-        .catch((error) => {
+        .catch(error => {
             console.log("Error de conexion: " + error.message);
             res.render('error', {error: "Error de conexion: " + error.message});
         });
